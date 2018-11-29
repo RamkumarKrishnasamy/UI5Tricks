7543bb6b4842949e277e0ee45da443c18f3b2f29
RamkumarKrishnasamy <40354751+RamkumarKrishnasamy@users.noreply.github.com>
2018-11-29 15:17:49 +1000

    Update controller.js

--- controller.js
+++ controller.js
@@ -8,3 +8,14 @@ var oDateRangePicker = this.getView().byId("daterangeSelector");
 					$('#' + oID).attr("disabled", "disabled");
 				}
 			}, oDateRangePicker);
+
+//If the view is read only, disable the upload
+if (!this.getView().getModel("viewModel").getProperty("/editable")) {
+	var sComp = this.getView().byId("attachments")
+		//Get the association to figure out the dynamic component id.
+		.getAssociation("component");
+	//Now construct the entire id string.
+	var sId = sComp + "---app--EmfUploadCollectionOther";
+	sap.ui.getCore().byId(sId).setUploadButtonInvisible(false);
+	sap.ui.getCore().byId(sId).setUploadEnabled(false);
+}
